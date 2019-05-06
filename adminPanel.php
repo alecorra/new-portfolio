07042f069a8310faa26db9bb1a07d387913feab1
Alessandro Corra <alecorra1980@gmail.com>
2019-05-06 15:42:54 +0100

    fixed admin panel

--- adminPanel.php
+++ adminPanel.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Alessandro Corrá / About me</title>
+    <title>Alessandro Corrá / ADMIN PANEL</title>
     <link rel="stylesheet" type="text/css" href="css/normalize.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -15,18 +15,8 @@
     <nav id="aboutMeNav" class="navbar">
         <img class="logo" src="img/MyLogo.png">
         <a class="home" href="index.html">HOME</a>
-        <a class="about-me active" href="aboutMe.html">ABOUT ME</a>
         <a class="portfolio" href="portfolio.php">PORTFOLIO</a>
-        <a class="contacts" href="#">CONTACTS</a>
-        <div class="nav-icons">
-            <a href="https://github.com/alecorra" target="_blank">
-                <i class="fa fa-github-square"> </i>
-            </a>
-            <a href="https://www.linkedin.com/in/alessandro-corr%C3%A1-ab4a45156/" target="_blank">
-                <i class="fa fa-linkedin-square"> </i>
-            </a>
-        </div>
-        <a class="small-text-links" href="#">Alessandro Corrá 2019</a>
+        <a class="add active" href="#">ADD NEW PROJECT</a>
     </nav>
 <!--===================================================== ADMIN PANEL-->
     <div class="admin-container">
